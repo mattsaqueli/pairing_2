ace0cf3fccb7c1cb30df1c264ff0c356434ebfc3
Matt Saqueli <msaqueli22@turing.edu>
2023-01-11 16:19:05 -0600

    Add arrays and places

--- friends.js
+++ friends.js
@@ -2,3 +2,7 @@ var arrays = ["Sarah", "Justin", "Mike", "Devon", "Matt"];
 console.log(arrays[2]);
 console.log(arrays[4]);
 console.log(arrays[0]);
+
+var places = ["Austin", "Denver", "Connecticut", "Maine", "NYC"];
+places.pop();
+console.log(places);
